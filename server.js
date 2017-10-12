d0b2f1477a4029496c9eca050a63510bb400cdeb
Pablo <jakematepablo@gmail.com>
2017-10-12 13:46:39 -0300

    the multiple list are working

--- server.js
+++ server.js
@@ -5,28 +5,105 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io').listen(server);
 
-// var screenNsp = io.of('/screen-game');
-// var mobileNsp = io.of('/mobile-game');
+var screenNsp = io.of('/screen-game');
+var mobileNsp = io.of('/mobile-game');
 
 var connections = [];
-var ServerSocket = null;
+var wordList = [[],[],[]];
+var totalCharacters= [0,0,0];
+var totalWords = [0,0,0];
+var screenConnected = false;
+
+var currentList = 0;
+
+var maxSize = 500;
+var minSize = 50;
+
+var addWordtoList = function(newWord, list){
+  totalWords[list] ++;
+  totalCharacters[list] += newWord.length;
+  console.log('new word: '+ newWord);
+  if ( wordList[list].length == 0){
+    wordList[list].push({word: newWord,freq: 1});
+  }
+  else{
+    var addWord = true;
+    for(var i = 0; i < wordList[list].length; i++){
+      if(!newWord.localeCompare(wordList[list][i].word)){
+        console.log("this word is already on the list ");
+        wordList[list][i].freq ++;
+        addWord = false;
+        i = wordList[list].length;
+      };
+    };
+    if(addWord) wordList[list].push({word: newWord,freq: 1});
+  };
+};
+
+var updateList = function(){
+    var list = [];
+    if(wordList[currentList].length != 0){
+      console.log(wordList[currentList]);
+      for(var i = 0; i < wordList[currentList].length; i++){
+        var word = wordList[currentList][i].word;
+        console.log(word);
+        var avaregeLength = totalCharacters[currentList]/totalWords[currentList];
+        var relativeLength = word.length/avaregeLength;
+        var size = wordList[currentList][i].freq/(totalWords[currentList]*relativeLength);
+        size = size*(maxSize-minSize)+minSize;
+        if (size*word.length > 3000)
+        size = 3000/word.length;
+        list.push([word,Math.round(size)]);
+      };
+    }
+    var msg = {wordSea: list, index: currentList};
+    screenNsp.emit('update',msg);
+    currentList = (currentList+1)%3;
+    console.log("sending msg");
+    setTimeout(function(){
+      updateList();
+      console.log("time to update");
+    },15000);
+};
 
 var ip_server = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080 ;
 
 //servir direcciones Web
 app.get('/formulario', function(req, res){
-  res.sendFile(__dirname+'formulario.html');
+  res.sendFile(__dirname+'/formulario.html');
 });
 app.get('/wordCloud', function(req, res){
-  res.sendFile(__dirname+'wordCloud.html');
+  res.sendFile(__dirname+'/wordCloud.html');
 });
 
-//screen Socket
-// screenNsp.on('connection',function(socket){
-//   console.log('screen connected');
-//   serverSocket = socket;
-// });
+app.use( '/js', express.static('js') );
+app.use( '/css', express.static('css') );
+app.use( '/assets', express.static('assets') );
+app.use( '/bower_modules', express.static('bower_modules') );
+
+screenNsp.on('connection',function(socket){
+    connections.push(socket);
+    if(!screenConnected)
+      updateList();
+      screenConnected = true;
+      ;
+
+    socket.on('disconnect', function(){
+		connections.splice(connections.indexOf(socket), 1 );
+  });
+});
+
+mobileNsp.on('connection',function(socket){
+  console.log('mobile connected');
+
+  socket.on('sendWord', function(msg){
+    console.log("recieving a new word");
+    addWordtoList(msg.word1,0);
+    addWordtoList(msg.word2,1);
+    addWordtoList(msg.word3,2);
+  });
+});
 
 server.listen(port, function(){
   console.log("Servidor en ejecución en el puerto "+ port);
